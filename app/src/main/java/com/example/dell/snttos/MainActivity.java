2035760bd57caa312375f61c53a8221fc6fdc650
Shubham Nagar <shubham.nagar_cs19@gla.ac.in>
2021-10-02 21:27:44 +0530

    hey its my first android application

---
+++ app/src/main/java/com/example/dell/snttos/MainActivity.java
@@ -0,0 +1,39 @@
+package com.example.dell.snttos;
+
+import android.speech.tts.TextToSpeech;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+import java.util.Locale;
+
+public class MainActivity extends AppCompatActivity {
+
+    EditText e1;
+    Button b1;
+    TextToSpeech ts;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        e1=(EditText)findViewById(R.id.editText);
+        b1=(Button)findViewById(R.id.button);
+        ts=new TextToSpeech(this, new TextToSpeech.OnInitListener() {
+            @Override
+            public void onInit(int i) {
+                ts.setLanguage(Locale.ENGLISH);
+                ts.setSpeechRate(0.9F);
+            }
+        });
+        b1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String s1=e1.getText().toString();
+                ts.speak(s1, TextToSpeech.QUEUE_FLUSH,null);
+            }
+        });
+    }
+}
